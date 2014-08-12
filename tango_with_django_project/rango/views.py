7c4b8d9e0fa68d3f74713b672ae0ccd9172e8703
Trey Cook <trey.cook@ssrc.msstate.edu>
2014-08-12 14:59:13 -0500

    Finished Chapter 7

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 from rango.models import Category, Page
 from rango.conversion import decode
+from rango.forms import CategoryForm, PageForm
+
 def index(request):
 	context = RequestContext(request)
 
@@ -22,7 +24,8 @@ def category(request, category_name_url):
 	context = RequestContext(request)
 
 	category_name = decode(category_name_url)
-	context_dict = {'category_name': category_name}
+	context_dict = {'category_name':category_name,
+					'category_name_url':category_name_url}
 
 	try:
 		category = Category.objects.get(name=category_name)
@@ -36,3 +39,51 @@ def category(request, category_name_url):
 		pass
 
 	return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+	context = RequestContext(request)
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+
+		else:
+			print form.errors
+
+	else:
+		form = CategoryForm()
+
+	return render_to_response('rango/add_category.html', {'form':form}, context)
+
+def add_page(request, category_name_url):
+	context = RequestContext(request)
+
+	category_name = decode(category_name_url)
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			page = form.save(commit=False)
+
+			try:
+				cat = Category.objects.get(name=category_name)
+				page.category = cat
+			except Category.DoesNotExist:
+				return render_to_response('rango/add_category.html', {}, context)
+
+			page.views = 0
+			page.save()
+
+			return category(request, category_name_url)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	return render_to_response( 'rango/add_page.html',
+		{'category_name_url':category_name_url,
+		'category_name':category_name, 'form':form},
+		context)
