1db1a0eeb301e552fa7892c4fdc95c5a7ee3cd16
Trey Cook <treyvon@treyvon-VirtualBox.(none)>
2014-08-14 21:22:45 -0500

    Finished first block of code in 10.5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,6 +8,8 @@ from rango.models import Category, Page
 from rango.conversion import decode
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
+from datetime import datetime
+
 def index(request):
 	context = RequestContext(request)
 
@@ -15,7 +17,26 @@ def index(request):
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
 
-	return render_to_response('rango/index.html', context_dict, context)
+	response = render_to_response('rango/index.html', context_dict, context)
+
+	# Get the number of visits to the site.
+	# COOKIES.get() function obtains the visits cookie.
+	visits = int(request.COOKIES.get('visits', '0'))
+
+	# Check for last_visit cookie
+	if 'last_visit' in request.COOKIES:
+		#It exists
+		last_visit = request.COOKIES['last_visit']
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+		if (datetime.now() - last_visit_time).days > 0:
+			response.set_cookie('visits', visits+1)
+			response.set_cookie('last_visit', datetime.now())
+
+	else:
+		response.set_cookie('last_visit', datetime.now())
+
+	return response
 
 def about(request):
 	context = RequestContext(request)
